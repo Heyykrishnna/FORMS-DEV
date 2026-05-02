a4b281672c1ad8cc175c4e5ed326555d5afad03a
Yatharth <khandelwalyatharth39@gmail.com>
2026-05-02 23:51:08 +0530

    templates

--- src/components/dashboard/TemplateGallery.tsx
+++ src/components/dashboard/TemplateGallery.tsx
@@ -44,11 +44,11 @@ const TemplateGallery = ({ isOpen, onClose, onSelect }: Props) => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
     }
     
     return () => {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
     };
   }, [isOpen]);
 
